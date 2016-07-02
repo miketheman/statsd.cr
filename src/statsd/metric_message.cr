5b446bf0a5bb15ad7a13be5ed91aabce2a6cc41c
Mike Fiedler <miketheman@gmail.com>
2016-07-02 07:46:14 -0400

    :art: Use Number::Primitive alias
    The pattern of using `value : Int::Primitive | Float::Primitive` is now
    replaceable with `Number::Primitive`, which is an in-language alias.
    
    See https://github.com/crystal-lang/crystal/commit/31f60d8a168763c1b6c204bf32da8cf577cf430e
    
    Add some doc notation for an example.

--- src/statsd/metric_message.cr
+++ src/statsd/metric_message.cr
@@ -3,18 +3,19 @@ module Statsd
     # Serialize the MetricMessage to a Datagram-formatted String
     #
     # Examples of valid datagrams format combinations:
+    # ```
     # metric.name:value|type
     # metric.name:value|type|@sample_rate
     # metric.name:value|type|#tag1:value,tag2
     # metric.name:value|type|@sample_rate|#tag1:value,tag2:value
+    # ```
 
     # :nodoc:
     def self.serialize_metric(
                               name : String,
-                              # TODO: replace with Number::Primitive in release post-0.16.0
-                              value : Int::Primitive | Float::Primitive,
+                              value : Number::Primitive,
                               metric_type : String,
-                              sample_rate : Int::Primitive | Float::Primitive | Nil = nil,
+                              sample_rate : Number::Primitive | Nil = nil,
                               tags : Array(String)? = nil)
       String.build do |io|
         io << name << ":" << value << "|" << metric_type
