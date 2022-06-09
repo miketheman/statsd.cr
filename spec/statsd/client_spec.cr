335c1e11ab59a78d594e07a394e2184064b7a25a
Carl Hörberg <carl@84codes.com>
2022-06-09 23:40:37 +0200

    Don't test setting host/port because it wasn't reflected anyway
    Resetting host/port wouldn't reset @destination, so an updated value was a noop.

--- spec/statsd/client_spec.cr
+++ spec/statsd/client_spec.cr
@@ -15,32 +15,6 @@ describe Statsd::Client do
     end
   end
 
-  describe "#host and #port" do
-    statsd = Statsd::Client.new
-
-    it "should set host and port" do
-      statsd.host = "1.2.3.4"
-      statsd.port = 5678
-      statsd.host.should eq "1.2.3.4"
-      statsd.port.should eq 5678
-    end
-
-    it "should set nil host to default" do
-      statsd.host = nil
-      statsd.host.should eq "127.0.0.1"
-    end
-
-    it "should set nil port to default" do
-      statsd.port = nil
-      statsd.port.should eq 8125
-    end
-
-    it "should allow an IPv6 address" do
-      statsd.host = "::1"
-      statsd.host.should eq "::1"
-    end
-  end
-
   describe "no server" do
     it "sends messages when nodoby is listening" do
       statsd = Statsd::Client.new(host: "127.0.0.1", port: 1234)
